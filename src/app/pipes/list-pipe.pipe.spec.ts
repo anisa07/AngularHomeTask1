39c2b205bcfe0ab3bf75ed5c04272b222bc8b6ba
anisa07 <askany07@gmail.com>
2018-12-20 22:07:25 +0300

    add pipes tests

--- src/app/pipes/list-pipe.pipe.spec.ts
+++ src/app/pipes/list-pipe.pipe.spec.ts
@@ -1,8 +1,17 @@
-import { ListPipe } from './list-pipe.pipe';
+import {ListPipe} from './list-pipe.pipe';
 
 describe('ListPipe', () => {
+  const pipe = new ListPipe();
+
   it('create an instance', () => {
-    const pipe = new ListPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('transform [1, 11, 3] and value 1 to [1, 11]', () => {
+    expect(pipe.transform([
+      {courseData: {title: 'aaa'}},
+      {courseData: {title: 'a'}},
+      {courseData: {title: 'aab'}},
+      ], 'aa')).toEqual([{courseData: {title: 'aaa'}}, {courseData: {title: 'aab'}}]);
   });
 });
